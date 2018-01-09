ddfffcca7379a1b8166cd23894119f8af125b283
Michael Sleeman <michael.k.sleeman@gmail.com>
2018-01-08 19:06:30 -0800

    Initial Commit: Git practice for team
    This is a MATLAB file where each member is meant to add a line that prints their name.
    
    Each member should pull the repository to their local repository, then they should change the code to print their name (as well as previous members names)
    
    Then they should write a commit message explaining the changes to the file and commit the file. After commiting, the local repository should be pushed to the remote repository

---
+++ git_practice.m
@@ -0,0 +1,10 @@
+% This code is meant to let members of Team 26 practice using git
+% Each member should pull this code to their local repository
+% Then they should add a line of code that prints the following to screen
+% 'My name is <team member's name>'
+% After adding this line of code, they should stage the file and write a
+% commit message explaining the changes they made to the code
+% After committing, they should push the commit to the remote repository on
+% GitHub
+
+fprint('My name is Michael')
