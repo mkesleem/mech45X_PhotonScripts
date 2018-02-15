2992f71acba745ad3fc4ae7a8f17aaf1f8521ea7
Michael Sleeman <michael.k.sleeman@gmail.com>
2018-02-15 13:47:11 -0800

    CO2 MHZ19 - added transistor

--- Arduino/MHZ19.cpp/MHZ19_main.ino
+++ Arduino/MHZ19.cpp/MHZ19_main.ino
@@ -1,14 +1,17 @@
 #include "MHZ19.h"
 MHZ19 myMHZ19;
-
+int mhz19_transistor_control = D0;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(mhz19_transistor_control,OUTPUT);
   Serial.println("-- Initializing...");
   delay(1000);
 }
 
 void loop() {
+  digitalWrite(mhz19_transistor_control, HIGH);
   myMHZ19.run_MHZ19();
+  digitalWrite(mhz19_transistor_control, LOW);
 }
 
