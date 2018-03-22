856a3640d31a05dfd9383767dd60cf03a7f97104
Michael Sleeman <michael.k.sleeman@gmail.com>
2018-03-22 11:57:59 -0700

    _all - added comments

--- Arduino/AllSensorsCombined/_all/_all.ino
+++ Arduino/AllSensorsCombined/_all/_all.ino
@@ -1,3 +1,4 @@
+#include "CALCULATE_MRT.h"
 #include "MHZ19.h"
 #include "CCS821.h"
 #include "SHT35D.h"
@@ -11,33 +12,44 @@ ClosedCube_Si7051 myMRT;
 ClosedCube_SHT31D mySHT;
 Adafruit_CCS811 myVOC;
 MHZ19 myCO2;
+mrt_and_ot my_MRT_OT;
 
-// boolean expressions
+/*
+ * Boolean expression
+ * Indicate whether sensor has
+ * been started succesfully
+ */
 bool start_co2 = false;
 bool start_voc = false;
 bool start_sht = false;
 bool start_pm = false;
 bool start_mrt = false;
-bool mhz19_average_check = false;
 
-// average values
+// average reading values
 int co2_ave = -1;
 float sht_rh_ave = -1;
 float sht_t_ave = -1;
 int voc_eCO2_ave = -1;
 int voc_TVOC_ave = -1;
 int voc_t_ave = -1;
-float mrt_t_ave = -1;
 float pm_ave = -1;
+float T_g = -1;
+float T_a = -1;
+float T_mrt = -1;
+float T_ot = -1;
 
-// should we publish data
-bool publish_data = true;
+bool publish_data = true; // should we publish data?
 
-// pins numbers
+// pin numbers for pm and co2 sensors
 int pm_transistor_control = A4;
 int co2_transistor_control = A3;
 
 void setup() {
+    /*
+     * start Serial and wire connections
+     * try to start each sensor
+     * assign true false to each of the relevant booleans
+     */
     Serial.begin(9600);
     Wire.begin();
     pinMode(pm_transistor_control,OUTPUT);
@@ -69,8 +81,12 @@ void setup() {
     Serial.println("-----------------------");
 }
 
-void loop()
-{
+void loop() {
+    /*
+     * Run each sensor if it has been started
+     * If the sensor has not been started, print error message
+     * After all values have been read, prepare to publish data
+     */
     if(start_co2) {
         digitalWrite(co2_transistor_control, HIGH);
         delay(1800);
@@ -130,67 +146,53 @@ void loop()
 
     if(publish_data) {
         char data[1000];
-        if(start_mrt){
-            mrt_t_ave = myMRT.get_MRT_ave();
-            
-            //sprintf(data,"{ \"MRT Temperature\": \"%f\"}" , mrt_t_ave);
-            //Particle.publish("MRT Temperature", data, PRIVATE);
+        if(start_mrt && start_sht){
+            T_g = myMRT.get_MRT_ave();
+            T_a = mySHT.get_t_ave();
+            sht_rh_ave = mySHT.get_rh_ave();
+            my_MRT_OT.calculate_mrt_and_ot(T_g, T_a);
+            T_mrt = my_MRT_OT.get_mrt();
+            T_ot = my_MRT_OT.get_ot();
         }
-        else
-        {
-            mrt_t_ave = -1;
-            //sprintf(data,"{ \"MRT Temperature\": \"%f\"}" , mrt_t_ave);
+        else if(start_mrt && !start_sht) {
+            T_g = myMRT.get_MRT_ave();
+            T_a = -1;
+            sht_rh_ave = -1;
+            T_mrt = -1;
+            T_ot = -1;
         }
-        if(start_pm){
-            pm_ave = myPM.getpm();
-            
-            //sprintf(data,"{ \"MRT Temperature\": \"%f\"}" , mrt_t_ave);
-            //Particle.publish("MRT Temperature", data, PRIVATE);
+        else if(!start_mrt && start_sht) {
+            T_g = -1;
+            T_a = mySHT.get_t_ave();
+            sht_rh_ave = mySHT.get_rh_ave();
+            T_mrt = -1;
+            T_ot = -1;
         }
-        else
-        {
-            pm_ave = -1;
-            //sprintf(data,"{ \"MRT Temperature\": \"%f\"}" , mrt_t_ave);
+        else {
+            T_g = -1;
+            T_a = -1;
+            sht_rh_ave = -1;
+            T_mrt = -1;
+            T_ot = -1;
         }
         
-        if(start_co2){
-            co2_ave = myCO2.get_co2_ave();
-            
-            //sprintf(data,"{ \"CO2 Concentration\": \"%i\"}" , co2_ave);
-            //Particle.publish("CO2 Concentration", data, PRIVATE);
-        }
-        else
-        {
-            co2_ave = -1;
-        }
+        if(start_pm){pm_ave = myPM.getpm();}
+        else {pm_ave = -1;}
+        
+        if(start_co2){co2_ave = myCO2.get_co2_ave();}
+        else{co2_ave = -1;}
         
         if(start_voc){
             voc_eCO2_ave = myVOC.get_eCO2_ave();
             voc_TVOC_ave = myVOC.get_TVOC_ave();
             voc_t_ave = myVOC.get_temp_ave();
-            
-           // sprintf(data,"{ \"VOC Equivalent CO2 Concentration\": \"%i\",\"TVOC\": \"%i\",\"VOC Temperature\": \"%i\" }" , voc_eCO2_ave,voc_TVOC_ave,voc_t_ave);
-            //Particle.publish("VOC Sensor", data, PRIVATE);
-        }
-        else
-        {
+        } else {
             voc_eCO2_ave = -1;
             voc_TVOC_ave = -1;
             voc_t_ave = -1;
         }
         
-        if(start_sht) {
-            sht_rh_ave = mySHT.get_rh_ave();
-            sht_t_ave = mySHT.get_t_ave();
-            
-            //sprintf(data,"{ \"SHT Temperature\": \"%f\",\"SHT Humidity\": \"%f\" }" , sht_rh_ave,sht_t_ave);
-            //Particle.publish("SHT RH/T", data, PRIVATE);
-        }
-        else{
-            sht_rh_ave = -1;
-            sht_t_ave = -1;
-        }
-        sprintf(data,"{ \"MRT Temperature\": \"%f\", \"CO2 Concentration\": \"%i\", \"VOC Equivalent CO2 Concentration\": \"%i\",\"TVOC\": \"%i\",\"PM 2.5 (Counts/m^3)\": \"%f\", \"SHT Temperature\": \"%f\",\"SHT Humidity\": \"%f\"}" , mrt_t_ave,co2_ave,voc_eCO2_ave,voc_TVOC_ave,pm_ave,sht_t_ave,sht_rh_ave);
+        sprintf(data,"{ \"Mean Radiant Temperature\": \"%f\", \"Operating Temperature\": \"%f\", \"Globe Temperature\": \"%f\", \"CO2 Concentration\": \"%i\", \"VOC Equivalent CO2 Concentration\": \"%i\",\"TVOC\": \"%i\",\"PM 2.5 (Counts/m^3)\": \"%f\", \"Air Temperature\": \"%f\",\"Relative Humidity of Air\": \"%f\"}" , T_mrt, T_ot, T_g, co2_ave, voc_eCO2_ave, voc_TVOC_ave, pm_ave, T_a, sht_rh_ave);
         Serial.println(data);
         Particle.publish("IEQ Data", data, PRIVATE);
     }
