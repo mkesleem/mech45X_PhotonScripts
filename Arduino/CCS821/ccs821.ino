a9a867c7fef3eb9f57ad3f2ec1897d62c70b9072
Michael Sleeman <michael.k.sleeman@gmail.com>
2018-03-30 15:00:28 -0700

    VOC - improved error handling and startup

--- Arduino/CCS821/ccs821.ino
+++ Arduino/CCS821/ccs821.ino
@@ -1,32 +1,28 @@
 #include "CCS821.h"
 
-int voc_transistor_control = D6;
 bool start_voc = false;
 
 Adafruit_CCS811 myVOC;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(voc_transistor_control,OUTPUT);
     delay(1000);
-    digitalWrite(voc_transistor_control, HIGH);
+    Serial.println("--------------------------");
+    Serial.println("Trying to start VOC sensor");
+    Serial.println("--------------------------");
     start_voc = myVOC.start_voc();
-    Serial.println("-----------------------");
-    digitalWrite(voc_transistor_control, LOW);
 }
 
 void loop() {
 if(start_voc) {
-        digitalWrite(voc_transistor_control, HIGH);
         Serial.println("Reading from VOC Sensor");
         Serial.println("-----------------------");
-        start_voc = myVOC.run_voc();
+        myVOC.run_voc();
         Serial.println("-----------------------");
-        digitalWrite(voc_transistor_control, LOW);
     }
     else if(!start_voc) {
         Serial.println("Not reading from VOC Sensor");
         Serial.println("---------------------------");
-        delay(500);
     }
+    delay(500);
 }
